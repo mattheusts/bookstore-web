415da324853612d7b0c2bd6769cf48a22099844b
matthaw <hardway3@protonmail.com>
2021-12-28 15:48:15 -0300

    feat: add dark theme

--- src/global/theme.ts
+++ src/global/theme.ts
@@ -1,4 +1,5 @@
-export default {
+const lightTheme = {
+  title: 'light',
   colors: {
     background: '#F0F2F5',
     shape: '#FFFFFF',
@@ -24,3 +25,33 @@ export default {
     bold: '600',
   },
 };
+
+const darkTheme = {
+  title: 'dark',
+  colors: {
+    background: '#222831',
+    shape: '#222831',
+    primary: '#FFD369',
+    yellow: '#ec8f00',
+    green: '#30475E',
+    blue: '#2589BD',
+    attention: '#F05454',
+    orange: '#FF934F',
+    title: '#EEEEEE',
+    text: '#969CB3',
+    grey: '#EEEEEE',
+    whish: '#AED6C9',
+    darkGreen: '#EEEEEE',
+    dark: '#EEEEEE',
+    black: '#000',
+  },
+
+  fonts: {
+    regular: '300',
+    extra_regular: '400',
+    medium: '500',
+    bold: '600',
+  },
+};
+
+export { lightTheme, darkTheme };

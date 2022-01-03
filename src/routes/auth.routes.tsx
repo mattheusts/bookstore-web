5b2719daa6296fdc3930569e631171c80bd33636
matthaw <hardway3@protonmail.com>
2022-01-03 15:44:28 -0300

    signup routes

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 import { SignIn } from '../pages/Auth/SignIn';
+import { SignUp } from '../pages/Auth/SignUp';
 import { Home } from '../pages/Home';
 import { NotFound } from '../pages/NotFound';
 
@@ -8,6 +9,7 @@ function AuthRoutes() {
   return (
     <Routes>
       <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/" element={<Home />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
